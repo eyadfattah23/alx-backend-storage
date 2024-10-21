463efee3b74593fe28f90e3bf16a8704b733b77f
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-10-21 14:47:09 +0300

    test one more fruit.

--- 0x00-MySQL_Advanced/4-main.sql
+++ 0x00-MySQL_Advanced/4-main.sql
@@ -23,6 +23,7 @@ SELECT * FROM orders;
 INSERT INTO orders (item_name, number) VALUES ('apple', 1);
 INSERT INTO orders (item_name, number) VALUES ('apple', 3);
 INSERT INTO orders (item_name, number) VALUES ('pear', 2);
+INSERT INTO orders (item_name, number) VALUES ('pineapple', 5);
 
 SELECT "--";
 
