d1274edfd2e34231830107f79413c2dd858a5317
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-12-10 15:43:09 +0200

    Create a Cache class. with a store method

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Define a Cache class."""
+
+import redis
+import uuid
+
+
+class Cache:
+    """Redis Cache class.
+    """
+
+    def __init__(self):
+        """initialize the Cache instance.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str | bytes | int | float) -> str:
+        """set a new key/value pair in the cache
+
+        Args:
+            data (str | bytes | int | float): data to be stored in the cache.
+
+        Returns:
+            str: uuid key of the new data stored in the cache.
+        """
+        new_key = str(uuid.uuid4())
+        self._redis.set(new_key, data)
+        return new_key
