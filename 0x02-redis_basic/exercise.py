db52ef6eff6edc00e7d1fe77ecb6f04b90a26024
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-12-10 16:05:16 +0200

    add get methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Awaitable, Any
 
 
 class Cache:
@@ -28,3 +28,55 @@ class Cache:
         new_key = str(uuid.uuid4())
         self._redis.set(new_key, data)
         return new_key
+
+    def get(self, key: str, fn: Callable = None) ->\
+            Union[Awaitable, Any, None]:
+        """get a key/value pair in the cache
+
+
+        Args:
+            key (str): key of the piece of data to retrieve
+            fn (Callable, optional): convert data back to the desired format.
+                                    Defaults to None.
+        """
+        return fn(self._redis.get(key)) if fn else self._redis.get(key)
+
+    def get_str(self, key: str, fn: Callable = None) ->\
+            Union[Awaitable, Any, None]:
+        """get a key/value pair in the cache
+
+
+        Args:
+            key (str): key of the piece of data to retrieve
+            fn (Callable, optional): convert data back to the desired format.
+                                    Defaults to None.
+        """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str, fn: Callable = None) ->\
+            Union[Awaitable, Any, None]:
+        """get a key/value pair in the cache
+
+
+        Args:
+            key (str): key of the piece of data to retrieve
+            fn (Callable, optional): convert data back to the desired format.
+                                    Defaults to None.
+        """
+        return self.get(key, int)
+
+
+""" Test Block 0
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
+"""
