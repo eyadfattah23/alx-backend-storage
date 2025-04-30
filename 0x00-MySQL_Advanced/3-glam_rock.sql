c920addb2b205a4258639bdcd06d1175d09c7a9e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-04-30 19:31:40 +0300

    add inits and new way for the same task

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,3 +4,9 @@ SELECT band_name, (IFNULL(CAST(split AS UNSIGNED), 2022) - CAST(formed AS UNSIGN
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
+
+
+-- SELECT band_name, (COALESCE(split, CAST(2022 AS YEAR)) - formed) AS dur, style
+-- FROM metal_bands
+-- WHERE style LIKE '%glam rock%'
+-- ORDER BY lifespan DESC;
